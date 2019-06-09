8ee57c38feba0262c1ee208d6ca3b159a0e17ca5
Jefferson Frade <jefferrson.frade@gmail.com>
2019-06-09 04:22:21 -0300

    Finished refactor

--- app/Repositories/Models/User.php
+++ app/Repositories/Models/User.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Repositories\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -26,14 +26,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
